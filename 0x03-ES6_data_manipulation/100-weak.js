a85cb8773771f382082066e8ca365b26e932b4b4
Chikawali <chykoblanko@gmail.com>
2024-07-02 06:57:36 -0700

    all done

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,9 +1,11 @@
+export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
-  if (!weakMap.has(endpoint)) {
-    weakMap.set(endpoint, 0);
-  }
-  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
-  if (weakMap.get(endpoint) >= MAX_ENDPOINT_CALLS) {
+  let count = weakMap.get(endpoint) || 0;
+  count += 1;
+
+  if (count >= 5) {
     throw new Error('Endpoint load is high');
+  } else {
+    weakMap.set(endpoint, count);
   }
 }
